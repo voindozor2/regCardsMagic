309cdf85cf49b3624e3ccbce0034cb0afe5bba91
VD2 <voindozor2@gmail.com>
2018-11-15 17:11:09 +0300

    возникает ошибка "IllegalArgumentException: A FileDescriptor instance with populated 'createDate' attribute must be provided"

--- modules/core/src/com/company/regofcardsmagic/core/ImportWorker.java
+++ modules/core/src/com/company/regofcardsmagic/core/ImportWorker.java
@@ -1,11 +1,33 @@
 package com.company.regofcardsmagic.core;
 
 import com.haulmont.cuba.core.entity.FileDescriptor;
+import com.haulmont.cuba.core.global.AppBeans;
+import com.haulmont.cuba.core.global.FileLoader;
+import com.haulmont.cuba.core.global.FileStorageException;
+import org.apache.poi.hssf.util.CellReference;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.InputStream;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+import javax.inject.Inject;
 
 @Component("regOfCardsMagic_ImportWorker")
 public class ImportWorker {
-    public void importAllCardsFromExcel (FileDescriptor fileDescriptor) {
+
+
+    public void importAllCardsFromExcel (FileDescriptor fileDescriptor) throws IOException,FileStorageException {
+        FileLoader fileLoader= AppBeans.get(FileLoader.class);
+        InputStream in = fileLoader.openStream(fileDescriptor);
+        HSSFWorkbook wb = new HSSFWorkbook(in);
+        Sheet sheet = wb.getSheetAt(0);
+        DataFormatter dataFormatter = new DataFormatter();
+
 
     }
 }
--- modules/web/src/com/company/regofcardsmagic/web/importFromExcel/Screen.java
+++ modules/web/src/com/company/regofcardsmagic/web/importFromExcel/Screen.java
@@ -2,12 +2,17 @@ package com.company.regofcardsmagic.web.importFromExcel;
 
 import com.company.regofcardsmagic.service.RegCardsService;
 import com.haulmont.cuba.core.entity.FileDescriptor;
+import com.haulmont.cuba.core.global.AppBeans;
+import com.haulmont.cuba.core.global.DataManager;
+import com.haulmont.cuba.core.global.FileLoader;
 import com.haulmont.cuba.core.global.FileStorageException;
 import com.haulmont.cuba.gui.components.*;
-import com.haulmont.cuba.gui.upload.FileUploadingAPI;
-
+    import com.haulmont.cuba.gui.upload.FileUploadingAPI;
 import javax.inject.Inject;
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
 
 public class Screen extends AbstractWindow {
@@ -28,10 +33,18 @@ public class Screen extends AbstractWindow {
     @Inject
     private RegCardsService regCardsService;
 
+    @Inject
+    private DataManager dataManager;
+
     @Override
     public void init(Map<String, Object> params) {
+
         upload.addFileUploadSucceedListener(event -> {
-            fd = upload.getFileDescriptor();
+            try {
+                fileUploadingAPI.putFileIntoStorage(upload.getFileId(),upload.getFileDescriptor());
+            } catch (FileStorageException e) {
+                throw new RuntimeException(e);
+            }
         });
 
         button.setAction(new AbstractAction("asd") {
@@ -40,10 +53,10 @@ public class Screen extends AbstractWindow {
                 try {
                     regCardsService.importFromExcel(fd);
                 }catch (IOException e) {
-
+               e.printStackTrace();
                 }
                 catch (FileStorageException e){
-
+                    e.printStackTrace();
                 }
 
                 }
